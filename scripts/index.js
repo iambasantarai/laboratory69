7987093be87b8081cb5eb6e3c02b9882a3bbcfef
iamsampang <iamsampang@gmail.com>
2023-11-14 20:29:21 +0545

    Implement copy to clipboard functionality

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,7 @@ const inputString = document.getElementById("inputString");
 const outputString = document.getElementById("outputString");
 const encodeButton = document.getElementById("encodeButton");
 const decodeButton = document.getElementById("decodeButton");
+const copyButton = document.getElementById("copyButton");
 
 encodeButton.addEventListener("click", () => {
   outputString.value = btoa(inputString.value);
@@ -11,4 +12,13 @@ encodeButton.addEventListener("click", () => {
 
 decodeButton.addEventListener("click", () => {
   outputString.value = atob(inputString.value);
+});
+
+copyButton.addEventListener("click", () => {
+  if (outputString.value) {
+    navigator.clipboard
+      .writeText(outputString.value)
+      .then(() => console.log("Copied to clipboard."))
+      .catch((error) => console.log("Failed to copy to clipboard: ", error));
+  }
 });
