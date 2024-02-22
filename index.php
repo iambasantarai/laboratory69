a5418dfa7810f4b053185f1b3e6bc1be1cd6ff2c
iambasantarai <iambasantarai@gmail.com>
2024-02-22 22:31:21 +0545

    Add prompt for user's name

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-    echo "Hello, World! 0/\n";
+
+    use function Laravel\Prompts\text;
+
+    require __DIR__ . "/vendor/autoload.php";
+     
+    $name = text("What is your name?");
+
+    echo "Hello, " . $name . ".\n";
 ?>
