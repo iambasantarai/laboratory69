08a35ebd4aa9945f2a13461acedde2f739d3f403
iambasantarai <iambasantarai@gmail.com>
2024-03-21 08:58:26 +0545

    Initial setup for discord bot

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-console.log("::: uwu :::");
-console.log("Hello, World 0/");
-console.log("::: uwu :::");
+import dotenv from "dotenv";
+import { Client, GatewayIntentBits } from "discord.js";
+
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  message.reply({
+    content: "Hey, I'm bhunte. BHAW BHAW!",
+  });
+});
+
+client.login(process.env.TOKEN);
