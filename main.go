ab57263c03847f344bb1f2eaddfc18042902f15b
iambasantarai <iambasantarai@gmail.com>
2024-05-16 07:48:54 +0545

    Access command line arguments

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
 func main() {
-   print("Hello, World 0/\n") 
+
+    if len(os.Args) != 3 {
+       log.Fatalf("Error! Got more than expected arguments.\n") 
+    }
+
+    fmt.Printf("(os.Args[0]): PROGRAM NAME     %s\n", os.Args[0]) 
+    fmt.Printf("(os.Args[1]): FIRST ARGUMENT   %s\n", os.Args[1]) 
+    fmt.Printf("(os.Args[2]): SECOND ARGUMENT  %s\n", os.Args[2]) 
 }
