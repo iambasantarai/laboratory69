ad9439ef43e3ec31ad9cd86973cf81018ac68a94
iambasantarai <iambasantarai@gmail.com>
2024-05-16 08:32:41 +0545

    Write into a file

--- main.go
+++ main.go
@@ -12,7 +12,24 @@ func main() {
        log.Fatalf("Error! Got more than expected arguments.\n") 
     }
 
-    fmt.Printf("(os.Args[0]): PROGRAM NAME     %s\n", os.Args[0]) 
-    fmt.Printf("(os.Args[1]): FIRST ARGUMENT   %s\n", os.Args[1]) 
-    fmt.Printf("(os.Args[2]): SECOND ARGUMENT  %s\n", os.Args[2]) 
+    noteWriter(os.Args[1], os.Args[2])
+}
+
+func noteWriter(title string, url string) {
+    filename := "/home/basanta/Code/termnote/termnote.md"
+    file, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666) 
+
+    if err != nil {
+        fmt.Println("Error! Operation failed.")
+        panic(err)
+    }
+
+    _, err = file.WriteString("[" + title + "]" + "(" + url +")" + "\n")
+
+    if err != nil {
+        fmt.Println("Error! Operation failed.")
+        panic(err)
+    }
+
+    defer file.Close()
 }
