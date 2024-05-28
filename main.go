cf94d6246b41141b42788f9967e9435fc9cf7b5f
iambasantarai <iambasantarai@gmail.com>
2024-05-28 18:17:28 +0545

    Create a prompt to select note type

--- main.go
+++ main.go
@@ -8,28 +8,75 @@ import (
 
 func main() {
     var (
+        noteType string
+        thought string
         title string
         url string
+        content string
     )
 
-    form := huh.NewForm(
+    optionsForm := huh.NewForm(
+        huh.NewGroup(
+            huh.NewSelect[string]().Title("What type of note would you like to create?").
+            Options(
+                huh.NewOption("Link", "link"),
+                huh.NewOption("Thought", "thought"),
+                huh.NewOption("Short Note", "shortNote"),
+            ).Value(&noteType),
+        ),
+    )
+
+    linkForm := huh.NewForm(
         huh.NewGroup(
             huh.NewInput().
-                Title("Title").
+                Title("Enter the title for the article link").
                 Value(&title),
             huh.NewInput().
-                Title("URL").
+                Title("Enter the URL of the article").
                 Value(&url),
         ),
     )
 
-    err := form.Run()
+    thoughtForm := huh.NewForm(
+        huh.NewGroup(
+            huh.NewText().
+                Title("Share your thought").
+                Value(&thought),
+        ),
+    )
+
+    shortNoteForm := huh.NewForm(
+        huh.NewGroup(
+            huh.NewInput().
+                Title("Enter the title for your short note").
+                Value(&title),
+            huh.NewText().
+                Title("Write the content of your short note").
+                Value(&content),
+        ),
+    )
+
+    err := optionsForm.Run()
     errorHandler(err)
 
-    noteWriter(title, url)
+    switch noteType {
+    case "link":
+        err := linkForm.Run()
+        errorHandler(err)
+    case "thought":
+        err := thoughtForm.Run()
+        errorHandler(err)
+    case "shortNote":
+        err := shortNoteForm.Run()
+        errorHandler(err)
+    default:
+        panic("Invalid Option!")
+    }
+
+    linkWriter(title, url)
 }
 
-func noteWriter(title string, url string) {
+func linkWriter(title string, url string) {
     filename := "/home/basanta/Code/termnote/termnote.md"
     file, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666) 
     errorHandler(err)
