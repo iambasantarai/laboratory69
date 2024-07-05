ac6542d2a957070d8df7ce96921412ed3e55ffe8
iambasantarai <iambasantarai@gmail.com>
2024-07-05 11:19:39 +0545

    Setup prettier for code formatting

--- bullMQ-arena/producer.js
+++ bullMQ-arena/producer.js
@@ -1,22 +1,21 @@
-const { Queue } = require("bullmq");
+const { Queue } = require('bullmq');
 
 const redisOptions = { host: 'localhost', port: 6379, password: 'root' };
 
 const ingestionQueue = new Queue('ingest-scrapped', {
-    connection: redisOptions
+  connection: redisOptions,
 });
 
 async function init() {
-    try {
-        const res = await ingestionQueue.add('Ingest scrapped content', 
-            { content: 'This content is scrapped from web.' }
-        );
+  try {
+    const res = await ingestionQueue.add('Ingest scrapped content', {
+      content: 'This content is scrapped from web.',
+    });
 
-        console.log("Job added to the queue for ingestion ", res.id);
-    } catch (error) {
-        console.error("Error adding job to the queue: ", error);
-    }
+    console.log('Job added to the queue for ingestion ', res.id);
+  } catch (error) {
+    console.error('Error adding job to the queue: ', error);
+  }
 }
 
 init();
-
--- bullMQ-arena/worker.js
+++ bullMQ-arena/worker.js
@@ -1,22 +1,22 @@
-const { Worker } = require("bullmq");
+const { Worker } = require('bullmq');
 
 const redisOptions = { host: 'localhost', port: 6379, password: 'root' };
 
 const workerHandler = async (job) => {
-    console.log(`Processing job ${job.id}`);
+  console.log(`Processing job ${job.id}`);
 
-    return new Promise((resolve) => 
-        setTimeout(() => {
-            resolve();
-            console.log("Content ingested to LLM.");
-        }, 5 * 1000)
-    );
-}
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve();
+      console.log('Content ingested to LLM.');
+    }, 5 * 1000),
+  );
+};
 
 const ingestionWorker = new Worker('ingest-scrapped', workerHandler, {
-    connection: redisOptions
+  connection: redisOptions,
 });
 
-ingestionWorker.on('error', error => {
-    console.error("Worker error: ", error);
+ingestionWorker.on('error', (error) => {
+  console.error('Worker error: ', error);
 });
