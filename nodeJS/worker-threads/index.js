510aadca5ed1720ed398e38ace29df7321cc584a
iambasantarai <iambasantarai@gmail.com>
2024-07-08 19:34:37 +0545

    Offloading a CPU-bound task with worker-threads

--- nodeJS/worker-threads/index.js
+++ nodeJS/worker-threads/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Worker } = require('worker_threads');
 
 const app = express();
 
@@ -8,20 +9,16 @@ app.get('/non-blocking', (req, res) => {
   res.status(200).send('Served by non-blocking route.');
 });
 
-function calculateCount() {
-  return new Promise((resolve, reject) => {
-    let counter = 0;
-    for (let i = 0; i < 20_000_000_000; i++) {
-      counter++;
-    }
-    resolve(counter);
-  });
-}
-
 app.get('/blocking', async (req, res) => {
-  const counter = await calculateCount();
+  const worker = new Worker('./worker.js');
 
-  res.status(200).send(`Result is ${counter}`);
+  worker.on('message', (data) => {
+    res.status(200).send(`Result is ${data}`);
+  });
+
+  worker.on('error', (msg) => {
+    res.status(404).send(`An error occurred: ${msg}`);
+  });
 });
 
 app.listen(PORT, () => {
