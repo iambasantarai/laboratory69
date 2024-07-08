8009337d6a8dd064ef5294105e70cf830f53065f
iambasantarai <iambasantarai@gmail.com>
2024-07-08 19:25:42 +0545

    Offloading a CPU-intensive task using promises

--- nodeJS/worker-threads/index.js
+++ nodeJS/worker-threads/index.js
@@ -8,11 +8,18 @@ app.get('/non-blocking', (req, res) => {
   res.status(200).send('Served by non-blocking route.');
 });
 
+function calculateCount() {
+  return new Promise((resolve, reject) => {
+    let counter = 0;
+    for (let i = 0; i < 20_000_000_000; i++) {
+      counter++;
+    }
+    resolve(counter);
+  });
+}
+
 app.get('/blocking', async (req, res) => {
-  let counter = 0;
-  for (let i = 0; i < 20_000_000_000; i++) {
-    counter++;
-  }
+  const counter = await calculateCount();
 
   res.status(200).send(`Result is ${counter}`);
 });
