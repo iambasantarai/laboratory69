0a760e23cb6b2f183ac89eed42248523aec43a97
iambasantarai <iambasantarai@gmail.com>
2024-07-11 19:07:05 +0545

    Simulate CPU intensive task

--- nodeJS/clusters/index.js
+++ nodeJS/clusters/index.js
@@ -3,6 +3,15 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.get('/heavy', (req, res) => {
+  let total = 0;
+  for (let i = 0; i < 5_000_000; i++) {
+    total++;
+  }
+  res.send(`The result of the CPU intensive task is ${total}\n`);
+});
+
 app.listen(PORT, () => {
+  console.log(`PID : ${process.pid}`);
   console.log(`Listening on http://localhost:${PORT}`);
 });
