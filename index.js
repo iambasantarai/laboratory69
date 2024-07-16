1840ec1442a89d37f3cdaa7a9aa3bcd56492761f
iambasantarai <iambasantarai@gmail.com>
2024-07-16 17:58:33 +0545

    Ask for the website link to crawl

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function sum(a, b) {
-  return a + b;
+const readline = require('node:readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+async function init() {
+  const link = await new Promise((resolve, reject) => {
+    rl.question('Enter a website link you want to crawl: ', resolve);
+  });
+  rl.close();
+
+  console.log(`Crawling ${link}.`);
 }
 
-module.exports = sum;
+init();
