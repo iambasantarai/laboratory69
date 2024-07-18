13dc71bb2506713f8b0ae888b21f06433a158237
iambasantarai <iambasantarai@gmail.com>
2024-07-18 11:57:11 +0545

    Create virtual environment, install dependencies and execute python script

--- nodeJS/execute-python/index.js
+++ nodeJS/execute-python/index.js
@@ -1,7 +1,75 @@
-const { spawn } = require('node:child_process');
+const fs = require("node:fs");
+const path = require("node:path");
+const { spawn, exec } = require('node:child_process');
 
-const pythonExecuter = spawn('python3', ['./main.py']);
 
-pythonExecuter.stdout.on('data', (data) => {
-    console.log(data.toString());
+function createVirtualEnv(callback) {
+    const venvPath = path.join(process.cwd(), 'python-scripts/venv');
+
+    if (!fs.existsSync(venvPath)) {
+        console.log('Creating virtual environment...');
+        exec(`python3 -m venv ${venvPath}`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error creating virtual environment: ${error}`);
+                return;
+            }
+
+            if (stderr) {
+                console.error(`stderr: ${stderr}`);
+            }
+
+            console.log(`stdout: ${stdout}`);
+            callback();
+        });
+    } else {
+        console.log('Virtual environment already exists.');
+
+        callback();
+    }
+}
+
+function pythonDependenciesInstaller(callback) {
+    const venvPath = path.join(process.cwd(), 'python-scripts/venv/bin/pip');
+
+    console.log('Installing dependencies...');
+    exec(`${venvPath} install -r python-scripts/requirements.txt`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error installing dependencies: ${error}`);
+            return;
+        }
+
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+        }
+
+        console.log(`stdout: ${stdout}`);
+
+        callback();
+    });
+}
+
+function executePythonScript() {
+
+    const venvPythonPath = path.join(process.cwd(), 'python-scripts/venv/bin/python');
+    const pythonScriptPath = path.join(process.cwd(), 'python-scripts/main.py');
+
+    console.log('Executing Python script...');
+    const pythonExecuter = spawn(venvPythonPath, [pythonScriptPath]);
+
+    pythonExecuter.stdout.on('data', (data) => {
+        console.log(data.toString());
+    });
+
+    pythonExecuter.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    pythonExecuter.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+}
+
+// Create virtual environment, install dependencies, and then execute the Python script
+createVirtualEnv(() => {
+    pythonDependenciesInstaller(executePythonScript);
 });
--- nodeJS/execute-python/python-scripts/main.py
+++ nodeJS/execute-python/python-scripts/main.py
@@ -1,7 +1,10 @@
 import os
 import platform
+import pyfiglet
 
 def system_fetch():
+    title = pyfiglet.figlet_format("System Information", font = "digital")
+    print(title)
     print("OS: ", os.name)
     print("System: ", platform.system())
     print("Architecture: ", platform.machine())
