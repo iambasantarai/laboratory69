6fc8b6e198387439a9be119db517b0eaeb752720
iambasantarai <iambasantarai@gmail.com>
2024-07-28 21:39:14 +0545

    Use continue and break in loops

--- golang/basics/loops.go
+++ golang/basics/loops.go
@@ -2,6 +2,33 @@ package main
 
 import "fmt"
 
+// using continue and break
+func printPrime(max int) {
+    for n := 2; n <= max + 1; n++ {
+        if n == 2 {
+            fmt.Println(n)
+            continue
+        }
+
+        if n % 2 == 0 {
+            continue
+        }
+
+        isPrime := true
+        for i := 3; i * i < n + 1; i++ {
+            if n % i == 0 {
+                isPrime = false
+                break
+            }
+        }
+
+        if !isPrime {
+            continue
+        }
+        fmt.Println(n)
+    }
+}
+
 func main() {
 
     for i := 0; i < 10; i++ {
@@ -15,4 +42,6 @@ func main() {
         yourAge++
     }
     fmt.Println("You can finally vote.")
+
+    printPrime(10)
 }
