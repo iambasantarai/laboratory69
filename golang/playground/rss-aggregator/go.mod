88b4014e01f555cfcd15de0819360a4795789987
iambasantarai <iambasantarai@gmail.com>
2024-08-21 08:55:25 +0545

    Read environment variables

--- golang/playground/rss-aggregator/go.mod
+++ golang/playground/rss-aggregator/go.mod
@@ -1,3 +1,5 @@
 module rss-aggregator
 
 go 1.22.5
+
+require github.com/joho/godotenv v1.5.1
--- golang/playground/rss-aggregator/main.go
+++ golang/playground/rss-aggregator/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+    "github.com/joho/godotenv"
+)
 
 func main() {
     fmt.Println("Hello world")
+
+
+    err := godotenv.Load()
+
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    portString := os.Getenv("PORT")
+
+    if portString == "" {
+        log.Fatal("Value for PORT not found in .env file")
+    }
+
+    fmt.Println("PORT: ", portString)
 }
