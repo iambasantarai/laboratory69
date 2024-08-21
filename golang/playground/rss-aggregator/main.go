a77411c9f61f2994197228b3554d82aa1a9949b1
iambasantarai <iambasantarai@gmail.com>
2024-08-21 19:52:15 +0545

    Finish creating user

--- golang/playground/rss-aggregator/main.go
+++ golang/playground/rss-aggregator/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
+	"rss-aggregator/internal/database"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+    _ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+    DB *database.Queries
+}
 
 func main() {
     err := godotenv.Load()
@@ -19,6 +27,20 @@ func main() {
     portString := os.Getenv("PORT")
     if portString == "" {
         log.Fatal("Value for PORT not found in .env file")
+    }
+
+    dbURL := os.Getenv("DB_URL")
+    if dbURL == "" {
+        log.Fatal("Value for DB_URL not found in .env file")
+    }
+
+    conn, err := sql.Open("postgres", dbURL)
+    if err != nil {
+        log.Fatal("Failed to connect to database: ", err)
+    }
+
+    apiCfg := apiConfig{
+        DB: database.New(conn),
     }
 
     router := chi.NewRouter()
@@ -35,6 +57,7 @@ func main() {
     v1Router := chi.NewRouter()
     v1Router.Get("/healthz", handlerReadiness)
     v1Router.Get("/error", handlerError)
+    v1Router.Post("/users", apiCfg.handlerCreateUser)
 
     router.Mount("/v1", v1Router)
 
