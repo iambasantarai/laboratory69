0b869606d87300f20b891b51aef4be0f06229c20
iambasantarai <iambasantarai@gmail.com>
2024-08-27 21:04:04 +0545

    Add api key for a user

--- golang/playground/rss-aggregator/internal/database/users.sql.go
+++ golang/playground/rss-aggregator/internal/database/users.sql.go
@@ -13,9 +13,8 @@ import (
 )
 
 const createUser = `-- name: CreateUser :one
-INSERT INTO users (id, created_at, updated_at, name) 
-VALUES ($1, $2, $3, $4)
-RETURNING id, created_at, updated_at, name
+INSERT INTO users (id, created_at, updated_at, name, api_key) 
+VALUES ($1, $2, $3, $4, encode(sha256(random()::text::bytea), 'hex')) RETURNING id, created_at, updated_at, name
 `
 
 type CreateUserParams struct {
@@ -32,6 +31,22 @@ func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, e
 		arg.UpdatedAt,
 		arg.Name,
 	)
+	var i User
+	err := row.Scan(
+		&i.ID,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.Name,
+	)
+	return i, err
+}
+
+const getUserByAPIKey = `-- name: GetUserByAPIKey :one
+SELECT id, created_at, updated_at, name FROM users WHERE 'api_key' = $1
+`
+
+func (q *Queries) GetUserByAPIKey(ctx context.Context, dollar_1 interface{}) (User, error) {
+	row := q.db.QueryRowContext(ctx, getUserByAPIKey, dollar_1)
 	var i User
 	err := row.Scan(
 		&i.ID,
