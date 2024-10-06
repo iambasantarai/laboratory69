2b91cf31ded149e1a4f14f1a89de11ea810b5622
iambasantarai <iambasantarai@gmail.com>
2024-10-06 21:29:15 +0545

    Different ways to deal with files

--- nodeJS/into-the-depths/fileSystem.js
+++ nodeJS/into-the-depths/fileSystem.js
@@ -1,5 +1,26 @@
+// const fs = require("node:fs");
+
+// const fileContent = fs.readFileSync("./data.txt");
+// console.log("CONTENT AS BUFFER: ", fileContent);
+// console.log("ACTUAL CONTENT: ", fileContent.toString("utf-8"));
+
+// Promise API
+// const fs = require("node:fs/promises");
+// (async () => {
+//   try {
+//     await fs.copyFile("./data.txt", "./copied-promise.txt");
+//   } catch (error) {
+//     console.log("ERROR: ", error);
+//   }
+// })();
+
+// Callback API
+// const fs = require("node:fs");
+//
+// fs.copyFile("./data.txt", "./copied-callback.txt", (error) => {
+//   if (error) console.log("ERROR: ", error);
+
+// Synchronous API
 const fs = require("node:fs");
 
-const fileContent = fs.readFileSync("./data.txt");
-console.log("CONTENT AS BUFFER: ", fileContent);
-console.log("ACTUAL CONTENT: ", fileContent.toString("utf-8"));
+fs.copyFileSync("./data.txt", "./copied-synchronous.txt");
