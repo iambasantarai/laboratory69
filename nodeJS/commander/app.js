afe66bbc3911d0fafbab7bc7acdbbfcddcec0816
iambasantarai <iambasantarai@gmail.com>
2024-10-12 18:17:46 +0545

    Clean up the code using event emitter

--- nodeJS/commander/app.js
+++ nodeJS/commander/app.js
@@ -6,24 +6,21 @@ const { Buffer } = require("node:buffer");
 
   const watcher = fs.watch("./command.txt");
 
+  commandFileHandler.on("change", async () => {
+    const size = (await commandFileHandler.stat()).size;
+    const fileBuff = Buffer.alloc(size);
+    const offset = 0;
+    const length = fileBuff.byteLength;
+    const position = 0;
+
+    await commandFileHandler.read(fileBuff, offset, length, position);
+
+    console.log(fileBuff.toString("utf-8"));
+  });
+
   for await (const event of watcher) {
     if (event.eventType === "change") {
-      console.log(event.filename + " is changed.");
-
-      const size = (await commandFileHandler.stat()).size;
-      const fileBuff = Buffer.alloc(size);
-      const offset = 0;
-      const length = fileBuff.byteLength;
-      const position = 0;
-
-      const content = await commandFileHandler.read(
-        fileBuff,
-        offset,
-        length,
-        position,
-      );
-
-      console.log(content);
+      commandFileHandler.emit("change");
     }
   }
 })();
