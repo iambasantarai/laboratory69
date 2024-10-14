070f3338c2eea7fb4083b860d4e12e42a6357737
iambasantarai <iambasantarai@gmail.com>
2024-10-14 19:26:57 +0545

    Implement delete, rename and add content to a file

--- nodeJS/commander/app.js
+++ nodeJS/commander/app.js
@@ -2,8 +2,15 @@ const fs = require("node:fs/promises");
 const { Buffer } = require("node:buffer");
 
 (async () => {
+  const CREATE_FILE = "create a file";
+  const DELETE_FILE = "delete the file";
+  const RENAME_FILE = "rename the file";
+  const ADD_CONTENT_TO_FILE = "add to the file";
+
   const createFile = async (path) => {
     try {
+      path = path.trim();
+
       const existingFileHandler = await fs.open(path, "r");
       existingFileHandler.close();
 
@@ -15,7 +22,57 @@ const { Buffer } = require("node:buffer");
     }
   };
 
-  const CREATE_FILE = "create a file";
+  const deleteFile = async (path) => {
+    try {
+      path = path.trim();
+
+      await fs.unlink(path);
+      console.log("File has been removed.");
+    } catch (error) {
+      if (error.code === "ENOENT") {
+        console.log("No file at this path to remove.");
+      } else {
+        console.log("Error removing file.");
+      }
+    }
+  };
+
+  const renameFile = async (oldPath, newPath) => {
+    try {
+      oldPath = oldPath.trim();
+      newPath = newPath.trim();
+
+      await fs.rename(oldPath, newPath);
+
+      console.log("File has been renamed.");
+    } catch (error) {
+      if (error.code === "ENOENT") {
+        console.log(
+          "No file at this path to rename or the destination doesn't exist.",
+        );
+      } else {
+        console.log("Error renaming file.");
+      }
+    }
+  };
+
+  let prevContent;
+  const addContentToFile = async (path, content) => {
+    if (prevContent === content) return;
+
+    try {
+      path = path.trim();
+
+      const fileHandle = await fs.open(path, "a");
+      fileHandle.write(content);
+      prevContent = content;
+      fileHandle.close();
+
+      console.log("Content has been added to file.");
+    } catch (error) {
+      console.log("ERROR: ", error);
+    }
+  };
 
   const commandFileHandler = await fs.open("./command.txt", "r");
 
@@ -32,9 +89,46 @@ const { Buffer } = require("node:buffer");
 
     const command = fileBuff.toString("utf-8");
 
+    /*
+     * CREATE FILE
+     * Example command: create a file <path>
+     */
     if (command.includes(CREATE_FILE)) {
       const filePath = command.substring(CREATE_FILE.length + 1);
       createFile(filePath);
+    }
+
+    /*
+     * DELETE FILE
+     * Example command: delete a file <path>
+     */
+    if (command.includes(DELETE_FILE)) {
+      const filePath = command.substring(DELETE_FILE.length + 1);
+      deleteFile(filePath);
+    }
+
+    /*
+     * RENAME FILE
+     * Example command: rename the file <old-path> to <new-path>
+     */
+    if (command.includes(RENAME_FILE)) {
+      const _idx = command.indexOf(" to ");
+      const oldFilePath = command.substring(RENAME_FILE.length + 1, _idx);
+      const newFilePath = command.substring(_idx + 4);
+
+      renameFile(oldFilePath, newFilePath);
+    }
+
+    /*
+     * ADD CONTENT TO FILE
+     * Example command: add to the file <path> this content: <content>
+     */
+    if (command.includes(ADD_CONTENT_TO_FILE)) {
+      const _idx = command.indexOf(" this content: ");
+      const filePath = command.substring(ADD_CONTENT_TO_FILE.length + 1, _idx);
+      const content = command.substring(_idx + 15);
+
+      addContentToFile(filePath, content);
     }
   });
 
