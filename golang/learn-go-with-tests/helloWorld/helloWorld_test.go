962cc7f404b0454698a4cb214a93552649caf823
iambasantarai <iambasantarai@gmail.com>
2024-10-31 20:23:09 +0545

    More powerful function

--- golang/learn-go-with-tests/helloWorld/helloWorld_test.go
+++ golang/learn-go-with-tests/helloWorld/helloWorld_test.go
@@ -4,15 +4,29 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Basanta")
+		got := Hello("Basanta", "")
 		want := "Hello, Basanta."
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, world.' if empty string is provided", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World."
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Bruce", "Spanish")
+		want := "Hola, Bruce."
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in french", func(t *testing.T) {
+		got := Hello("Bruce", "French")
+		want := "Bonjour, Bruce."
 
 		assertCorrectMessage(t, got, want)
 	})
