4e27962466ad4f112fcb3f16446785fc2131f847
iambasantarai <iambasantarai@gmail.com>
2024-11-02 20:31:35 +0545

    Get sum of array with any size

--- golang/learn-go-with-tests/arrays-and-slices/sum.go
+++ golang/learn-go-with-tests/arrays-and-slices/sum.go
@@ -1,13 +1,10 @@
 package arraysandslices
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
-}
-
-func main() {
 }
