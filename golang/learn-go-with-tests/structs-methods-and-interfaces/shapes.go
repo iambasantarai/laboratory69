b34f2651e3b8c9679523b909cd31354b9978d0ee
iambasantarai <iambasantarai@gmail.com>
2024-11-03 20:09:55 +0545

    Use of struct

--- golang/learn-go-with-tests/structs-methods-and-interfaces/shapes.go
+++ golang/learn-go-with-tests/structs-methods-and-interfaces/shapes.go
@@ -1,9 +1,14 @@
 package structsmethodsandinterfaces
 
-func Perimeter(l, b float64) float64 {
-	return 2 * (l + b)
+type Rectangle struct {
+	Width  float64
+	Height float64
 }
 
-func Area(l, b float64) float64 {
-	return (l * b)
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return (rectangle.Height * rectangle.Width)
 }
--- golang/learn-go-with-tests/structs-methods-and-interfaces/shapes_test.go
+++ golang/learn-go-with-tests/structs-methods-and-interfaces/shapes_test.go
@@ -3,8 +3,19 @@ package structsmethodsandinterfaces
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
+
+	if got != want {
+		t.Errorf("expected '%.2f', got '%.2f'", want, got)
+	}
+}
+
+func TestArea(t *testing.T) {
+	rectangle := Rectangle{2.0, 10.0}
+	got := Area(rectangle)
+	want := 20.0
 
 	if got != want {
 		t.Errorf("expected '%.2f', got '%.2f'", want, got)
