dc5005f5d257bda2e318c350fdd09fa0c46b0545
iambasantarai <iambasantarai@gmail.com>
2025-01-11 20:06:05 +0545

    Display word filtered by length

--- bash/task1.sh
+++ bash/task1.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 rand_word=$(cat ./data.txt | shuf -n 1)
-echo $rand_word
+echo "random word: $rand_word"
+
+word_len=$1
+filtered_word=$(grep -E "^.{${word_len}}$" ./data.txt | shuf -n 1)
+echo "$filtered_word"
