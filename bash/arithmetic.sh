d0ed8c50fb8f3041bc89564c313b14fda85f6e60
iambasantarai <iambasantarai@gmail.com>
2025-01-12 18:53:16 +0545

    Using expr

--- bash/arithmetic.sh
+++ bash/arithmetic.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 let sum=5+5
-echo sum: $sum
-let product=$sum*$1
+let product=$sum\*$1
+difference=$(expr $sum - $1)
 
+echo sum: $sum
 echo "$sum multiplied by $1 is $product"
+echo "$sum minus $1 is $difference"
