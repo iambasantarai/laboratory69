449a8b1859a172f3cb85804af5dbd3f333ee684e
iambasantarai <iambasantarai@gmail.com>
2025-01-15 21:48:17 +0545

    Use break & continue in loops

--- bash/for_loop.sh
+++ bash/for_loop.sh
@@ -16,11 +16,19 @@ done
 echo
 echo It is also possible, steps in a loop
 for j in {69..0..5}; do
+    if [ $j -eq 39 ]; then
+        echo we reached $j as well
+        continue
+    fi
     echo $j
 done
 
 echo
 echo A range alternative
 for ((num = 1; num <= 5; num++)); do
+    if [ $num -eq 2 ]; then
+        echo $num
+        break
+    fi
     echo $num
 done
