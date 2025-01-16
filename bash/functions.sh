9b610397ab450d2740f36577be3f4e09762a51cd
iambasantarai <iambasantarai@gmail.com>
2025-01-16 21:22:29 +0545

    Use function return values

--- bash/functions.sh
+++ bash/functions.sh
@@ -7,3 +7,10 @@ greet() {
 greet Namaste John
 greet Sewa Layla
 greet Ola Cristin
+
+file_lines_counter() {
+    cat $1 | wc -l
+}
+
+lines=$(file_lines_counter $1)
+echo The file $1 has $lines no of lines in it.
