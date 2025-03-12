1a8dcc59aaf8d0b40c9d87c30f79b549d87d513a
iambasantarai <developer.basanta@gmail.com>
2025-03-12 21:44:22 +0545

    Implement create, get and list account API with Gin

--- golang/simple-bank/main.go
+++ golang/simple-bank/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+
+	"simple-bank/api"
+	db "simple-bank/db/sqlc"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://postgres:postgres@localhost/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
 
 func main() {
-	fmt.Println("simple bank")
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("can not connect to db: ", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("can not start server: ", err)
+	}
 }
