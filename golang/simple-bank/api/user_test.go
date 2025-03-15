709c54f52fd7d5facf01ebcd0d9b4d2571035d9a
iambasantarai <developer.basanta@gmail.com>
2025-03-15 19:48:45 +0545

    Improve unit test with custom gomock matcher

--- golang/simple-bank/api/user_test.go
+++ golang/simple-bank/api/user_test.go
@@ -4,9 +4,11 @@ import (
 	"bytes"
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -18,6 +20,45 @@ import (
 	db "simple-bank/db/sqlc"
 	"simple-bank/util"
 )
+
+type eqCreateUserParamsMatcher struct {
+	arg      db.CreateUserParams
+	password string
+}
+
+func (e eqCreateUserParamsMatcher) Matches(x interface{}) bool {
+	arg, ok := x.(db.CreateUserParams)
+	if !ok {
+		return false
+	}
+
+	err := util.CheckPassword(e.password, arg.HashedPassword)
+	if err != nil {
+		return false
+	}
+
+	e.arg.HashedPassword = arg.HashedPassword
+	return reflect.DeepEqual(e.arg, arg)
+
+	// Check if types assignable and convert them to common type
+	// x1Val := reflect.ValueOf(e.arg)
+	// x2Val := reflect.ValueOf(x)
+	//
+	// if x1Val.Type().AssignableTo(x2Val.Type()) {
+	// 	x1ValConverted := x1Val.Convert(x2Val.Type())
+	// 	return reflect.DeepEqual(x1ValConverted.Interface(), x2Val.Interface())
+	// }
+	//
+	// return false
+}
+
+func (e eqCreateUserParamsMatcher) String() string {
+	return fmt.Sprintf("matches arg %v and password %v", e.arg, e.password)
+}
+
+func EqCreateUserParams(arg db.CreateUserParams, password string) gomock.Matcher {
+	return eqCreateUserParamsMatcher{arg: arg, password: password}
+}
 
 func TestCreateUserAPI(t *testing.T) {
 	user, password := randomUser(t)
@@ -73,8 +114,14 @@ func TestCreateUserAPI(t *testing.T) {
 				"email":     user.Email,
 			},
 			buildStubs: func(store *mockdb.MockStore) {
+				arg := db.CreateUserParams{
+					Username: user.Username,
+					FullName: user.FullName,
+					Email:    user.Email,
+				}
+
 				store.EXPECT().
-					CreateUser(gomock.Any(), gomock.Any()).
+					CreateUser(gomock.Any(), EqCreateUserParams(arg, password)).
 					Times(1).
 					Return(db.User{}, &pq.Error{Code: "23505"})
 			},
